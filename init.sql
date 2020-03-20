8f0e84f0babf2b724678fd7b6ed4b8590982bc7d
Ong Ka Yi <e0335691@u.nus.edu>
2020-03-20 18:36:06 +0800

    Init.sql edited + Some fake data inserted from Kayi side

--- init.sql
+++ init.sql
@@ -115,7 +115,7 @@ CREATE TABLE promotion (
 
 CREATE TABLE restaurantFood (
 	price FLOAT NOT NULL,
-	name varchar(100) UNIQUE,
+	name varchar(100),
 	category varchar(100) NOT NULL,
 	information varchar(500),
 	availabilityStatus BOOLEAN DEFAULT FALSE,
@@ -190,6 +190,7 @@ CREATE TABLE delivery (
 	deliveryId integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	deliveryLocation varchar(500) NOT NULL,
 	customerId integer,
+	orderedTimestamp timestamp,
 	FOREIGN KEY (customerId) REFERENCES customers
 );
 
@@ -200,6 +201,67 @@ CREATE TABLE pointTransaction (
 	customerId integer,
 	FOREIGN KEY (customerId) REFERENCES customers 
 );
+
+INSERT INTO restaurant(name, contactNo, address, area, minMonetaryAmount) values('Rochor Beancurd', '63723101', '787 Geylang Road S389660', 'East', 8);
+INSERT INTO restaurantFood(price, name, category, information, availabilityStatus, restaurantId, dailyLimit) values(2.5, 'Soya Beancurd', 'Dessert', 'Homemade soya beancurd, freshly made daily', true, 1, 100);
+INSERT INTO restaurantFood(price, name, category, information, availabilityStatus, restaurantId, dailyLimit) values(3.5, 'Soya Beancurd with Pearls', 'Dessert', 'Homemade soya beancurd, freshly made daily with pearls', true, 1, 150);
+
+INSERT INTO restaurant(name, contactNo, address, area, minMonetaryAmount) values('Popeyes', '63723102', '23 Kallang Wave Road S298102', 'East', 15);
+INSERT INTO restaurantFood(price, name, category, information, availabilityStatus, restaurantId, dailyLimit) values(5.0, '2 Pcs Chicken Set', 'Fast Food', 'Fried upon order, with Popeyes secret batter', true, 2, 130);
+
+INSERT INTO restaurant(name, contactNo, address, area, minMonetaryAmount) values('Texas', '63723103', '45 Kallang Wave Road S298119', 'East', 15);
+INSERT INTO restaurantFood(price, name, category, information, availabilityStatus, restaurantId, dailyLimit) values(5.5, '2 Pcs Chicken Set', 'Fast Food', 'Fried upon order, with Texas secret batter', true, 3, 90);
+
+INSERT INTO restaurant(name, contactNo, address, area, minMonetaryAmount) values('Playmade', '63723104', '22 Upper Paya Lebar Road S380290', 'East', 5);
+INSERT INTO restaurantFood(price, name, category, information, availabilityStatus, restaurantId, dailyLimit) values(3.5, 'Chrysanthemum Tea', 'Bubble Tea', 'Freshly brewed chrysanthemum tea', true, 4, 200);
+
+INSERT INTO users(name, username, password, contactNo) values('Lee Xiao Long', 'leexl', 'password', '81110111');
+INSERT INTO users(name, username, password, contactNo) values('Lee Xiao Bin', 'leexb', 'password', '81110112');
+INSERT INTO users(name, username, password, contactNo) values('Lee Xiao Kun', 'leexk', 'password', '81110113');
+
+INSERT INTO customers(accumulatedPoints, userId) values(100, 1);
+INSERT INTO customers(accumulatedPoints, userId) values(200, 2);
+INSERT INTO customers(accumulatedPoints, userId) values(300, 3);
+
+INSERT INTO orders(totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
+values(70, '2019-6-20 14:23:54', '234 Seng Keng Avenue 3 #21-14 S201010', 10);
+INSERT INTO delivery(deliveryLocation, orderedTimestamp) values('234 Seng Keng Avenue 3 #21-14 S201010', current_timestamp);
+INSERT INTO stores(customerId, deliveryId) values (2, 3);
+
+INSERT INTO orders(totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
+values(50, '2019-5-19 10:23:54', '123 Barney Road #01-04 S101010', 5);
+INSERT INTO delivery(deliveryLocation, orderedTimestamp) values('123 Barney Road #01-04 S101010', current_timestamp);
+INSERT INTO stores(customerId, deliveryId) values (1, 1);
+
+INSERT INTO orders(totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
+values(60, '2019-5-20 12:23:54', '123 Barney Road #01-04 S101010', 5);
+INSERT INTO delivery(deliveryLocation, orderedTimestamp) values('123 Barney Road #01-04 S101010', current_timestamp);
+INSERT INTO stores(customerId, deliveryId) values (1, 2);
+
+INSERT INTO orders(totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
+values(100, '2019-5-24 12:23:54', '34 Computing Drive #4-21 S301010', 8);
+INSERT INTO delivery(deliveryLocation, orderedTimestamp) values('34 Computing Drive #4-21 S301010', current_timestamp);
+INSERT INTO stores(customerId, deliveryId) values (1, 4);
+
+INSERT INTO orders(totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
+values(80, '2019-5-26 12:23:54', '42 Clementi Road S103391', 8);
+INSERT INTO delivery(deliveryLocation, orderedTimestamp) values('42 Clementi Road S103391', current_timestamp);
+INSERT INTO stores(customerId, deliveryId) values (1, 5);
+
+INSERT INTO orders(totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
+values(20, '2019-6-10 12:23:54', '34 Kim Cheng Street #01-21 S160110', 4);
+INSERT INTO delivery(deliveryLocation, orderedTimestamp) values('34 Kim Cheng Street #01-21 S160110', current_timestamp);
+INSERT INTO stores(customerId, deliveryId) values (1, 6);
+
+INSERT INTO orders(totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
+values(50, '2019-10-10 20:23:54', '139 Boon Tiong Road S169920', 4);
+INSERT INTO delivery(deliveryLocation, orderedTimestamp) values('139 Boon Tiong Road S169920', current_timestamp);
+INSERT INTO stores(customerId, deliveryId) values (1, 7);
+
+INSERT INTO orders(totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
+values(100, '2019-10-21 15:23:54', '795 Geylang Road #01-01 S389678', 7);
+INSERT INTO delivery(deliveryLocation, orderedTimestamp) values('795 Geylang Road #01-01 S389678', current_timestamp);
+INSERT INTO stores(customerId, deliveryId) values (1, 8);
 
 /*
 INSERT INTO restaurant(restaurantId, name, contactNo, address, area, minMonetaryAmount) values(10, 'Rochor Beancurd', '63723101', '787 Geylang Road S389660', 'East', 8);
@@ -225,41 +287,41 @@ INSERT INTO customers(customerId, accumulatedPoints, userId) values(3, 300, 3);
 
 INSERT INTO orders(orderId, totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
 values(3, 70, '2019-6-20 14:23:54', '234 Seng Keng Avenue 3 #21-14 S201010', 10);
-INSERT INTO delivery(deliveryId, deliveryLocations, orderedTimestamp) values(3, '234 Seng Keng Avenue 3 #21-14 S201010', current_timestamp);
+INSERT INTO delivery(deliveryId, deliveryLocation, orderedTimestamp) values(3, '234 Seng Keng Avenue 3 #21-14 S201010', current_timestamp);
 INSERT INTO stores(customerId, deliveryId) values (2, 3);
 
 INSERT INTO orders(orderId, totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
 values(1, 50, '2019-5-19 10:23:54', '123 Barney Road #01-04 S101010', 5);
-INSERT INTO delivery(deliveryId, deliveryLocations, orderedTimestamp) values(1, '123 Barney Road #01-04 S101010', current_timestamp);
+INSERT INTO delivery(deliveryId, deliveryLocation, orderedTimestamp) values(1, '123 Barney Road #01-04 S101010', current_timestamp);
 INSERT INTO stores(customerId, deliveryId) values (1, 1);
 
 INSERT INTO orders(orderId, totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
 values(2, 60, '2019-5-20 12:23:54', '123 Barney Road #01-04 S101010', 5);
-INSERT INTO delivery(deliveryId, deliveryLocations, orderedTimestamp) values(2, '123 Barney Road #01-04 S101010', current_timestamp);
+INSERT INTO delivery(deliveryId, deliveryLocation, orderedTimestamp) values(2, '123 Barney Road #01-04 S101010', current_timestamp);
 INSERT INTO stores(customerId, deliveryId) values (1, 2);
 
 INSERT INTO orders(orderId, totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
 values(4, 100, '2019-5-24 12:23:54', '34 Computing Drive #4-21 S301010', 8);
-INSERT INTO delivery(deliveryId, deliveryLocations, orderedTimestamp) values(4, '34 Computing Drive #4-21 S301010', current_timestamp);
+INSERT INTO delivery(deliveryId, deliveryLocation, orderedTimestamp) values(4, '34 Computing Drive #4-21 S301010', current_timestamp);
 INSERT INTO stores(customerId, deliveryId) values (1, 4);
 
 INSERT INTO orders(orderId, totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
 values(5, 80, '2019-5-26 12:23:54', '42 Clementi Road S103391', 8);
-INSERT INTO delivery(deliveryId, deliveryLocations, orderedTimestamp) values(5, '42 Clementi Road S103391', current_timestamp);
+INSERT INTO delivery(deliveryId, deliveryLocation, orderedTimestamp) values(5, '42 Clementi Road S103391', current_timestamp);
 INSERT INTO stores(customerId, deliveryId) values (1, 5);
 
 INSERT INTO orders(orderId, totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
 values(6, 20, '2019-6-10 12:23:54', '34 Kim Cheng Street #01-21 S160110', 4);
-INSERT INTO delivery(deliveryId, deliveryLocations, orderedTimestamp) values(6, '34 Kim Cheng Street #01-21 S160110', current_timestamp);
+INSERT INTO delivery(deliveryId, deliveryLocation, orderedTimestamp) values(6, '34 Kim Cheng Street #01-21 S160110', current_timestamp);
 INSERT INTO stores(customerId, deliveryId) values (1, 6);
 
 INSERT INTO orders(orderId, totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
 values(7, 50, '2019-10-10 20:23:54', '139 Boon Tiong Road S169920', 4);
-INSERT INTO delivery(deliveryId, deliveryLocations, orderedTimestamp) values(7, '139 Boon Tiong Road S169920', current_timestamp);
+INSERT INTO delivery(deliveryId, deliveryLocation, orderedTimestamp) values(7, '139 Boon Tiong Road S169920', current_timestamp);
 INSERT INTO stores(customerId, deliveryId) values (1, 7);
 
 INSERT INTO orders(orderId, totalOrderCost, orderDateTime, deliveryLocation, deliveryFee) 
 values(8, 100, '2019-10-21 15:23:54', '795 Geylang Road #01-01 S389678', 7);
-INSERT INTO delivery(deliveryId, deliveryLocations, orderedTimestamp) values(8, '795 Geylang Road #01-01 S389678', current_timestamp);
+INSERT INTO delivery(deliveryId, deliveryLocation, orderedTimestamp) values(8, '795 Geylang Road #01-01 S389678', current_timestamp);
 INSERT INTO stores(customerId, deliveryId) values (1, 8);
 */
